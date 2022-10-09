09af5c704a5771796813b72ae56d942f8d779f2a
LimJunseok <98313823+limjunseok0929@users.noreply.github.com>
2022-10-09 13:08:49 +0900

    Print ~ Var

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	/* Print */
+	fmt.Println("Hello World")
+
+	/* Input */
+	var a int
+	fmt.Scanln(&a)
+	fmt.Println(a)
+
+	/* Var */
+	name := "Junseok Lim" // = var name string = "Junseok Lim"
+	fmt.Println(name)
+}
